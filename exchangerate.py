3f5e2bd23176abc195a96184556b8b8d436f87bd
Vladislav <vladislav0904@gmail.com>
2020-11-01 18:59:23 +0500

    Cleared the code and made functions of the raw data in order to make regular update of csv

--- exchangerate.py
+++ exchangerate.py
@@ -5,44 +5,44 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
-data = pd.read_html("https://www.finanz.ru/indeksi/diagramma-realnogo-vremeni/micex", header=0, encoding='utf-8')
-string = str(data[3]).strip()
-string = 'Наименование Курс Изменение Время' + '\n' + string
-string = string.split('\n')
 
-for i in range(len(string)):
-    if i == 0:
-        pass
-    elif i == 1:
-        string[i] = string[i][:len(string[i]) - 24]
-    else:
-        string[i] = string[i][:len(string[i]) - 24][2:].lstrip()
-data = []
-for i in range(len(string)):
-    data.append(string[i].split())
-for i in data:
-    if data.index(i) != 0:
-        if i[1] != '500' and i[1].isdigit():
+def scrap_the_data():
+    data = pd.read_html("https://www.finanz.ru/indeksi/diagramma-realnogo-vremeni/micex", header=0, encoding='utf-8')
+    string = str(data[3]).strip()
+    string = 'Наименование Курс Изменение Время' + '\n' + string
+    string = string.split('\n')
+
+    for i in range(len(string)):
+        if i == 0:
             pass
+        elif i == 1:
+            string[i] = string[i][:len(string[i]) - 24]
         else:
-            i[0] = f'{i[0]} {i[1]}'
-            del i[1]
-        if i[1].isdigit() and (',' in i[2] and '%' not in i[2]):
-            i[1] = f'{i[1]} {i[2]}'
-            del i[2]
-        if i[1].isdigit():
-            i[1] = i[1][:2] + ',' + i[1][2:]
+            string[i] = string[i][:len(string[i]) - 24][2:].lstrip()
+    data = []
+    for i in range(len(string)):
+        data.append(string[i].split())
+    for i in data:
+        if data.index(i) != 0:
+            if i[1] != '500' and i[1].isdigit():
+                pass
+            else:
+                i[0] = f'{i[0]} {i[1]}'
+                del i[1]
+            if i[1].isdigit() and (',' in i[2] and '%' not in i[2]):
+                i[1] = f'{i[1]} {i[2]}'
+                del i[2]
+            if i[1].isdigit():
+                i[1] = i[1][:2] + ',' + i[1][2:]
+    return data
 
 
-with open('price.csv', 'w', newline='', encoding='Windows-1251') as csvfile:
-    writer = csv.writer(
-        csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    for i in range(len(data)):
-        writer.writerow(data[i])
-
-for i in range(len(data)):
-    data[i] = '    '.join(data[i])
-print('\n'.join(data))
+def get_csv(data):
+    with open('price.csv', 'w', newline='', encoding='Windows-1251') as csvfile:
+        writer = csv.writer(
+            csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for i in range(len(data)):
+            writer.writerow(data[i])
 
 
 class MyWidget(QMainWindow):
@@ -67,6 +67,7 @@ class MyWidget(QMainWindow):
                         i, j, QTableWidgetItem(elem))
 
 
+get_csv(scrap_the_data())
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyWidget()
